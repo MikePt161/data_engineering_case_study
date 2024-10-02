6edc016689e8ac3b16124bdcfffdbbe338e206e6
Michael Petrouleas <michaelpetrouleas@gmail.com>
2024-10-02 20:23:38 +0300

    task 1 : removed obsolette lines and hadoop specs

--- src/output_1.py
+++ src/output_1.py
@@ -1,4 +1,3 @@
-from Tools.scripts.generate_opcode_h import header
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 from pyspark import SparkConf
@@ -10,16 +9,6 @@ import importlib
 import utility.test_functions
 importlib.reload(utility.test_functions)
 from utility.test_functions import *
-
-import os
-
-# Set the Hadoop home directory
-os.environ['HADOOP_HOME'] = r'C:\Users\user\Documents\Projects\winutils\hadoop-3.3.5'  # Make sure to use your correct path
-os.environ['hadoop.home.dir'] = r'C:\Users\user\Documents\Projects\winutils\hadoop-3.3.5\bin\winutils.exe'
-
-conf = (SparkConf().
-        # set("spark.hadoop.io.nativeio.enable", "false").
-        set("spark.local.dir", r"C:\Users\user\Documents\Projects\data_engineering_case_study\temp"))
 
 def produce_it_data(dataset_one_path, dataset_two_path, spark_session):
 
@@ -40,7 +29,7 @@ def produce_it_data(dataset_one_path, dataset_two_path, spark_session):
 
 def main(dataset_one_path = r'../data/dataset_one.csv', dataset_two_path = r'../data/dataset_two.csv'):
 
-    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
 
     try:
 
@@ -59,25 +48,5 @@ def main(dataset_one_path = r'../data/dataset_one.csv', dataset_two_path = r'../
         spark.stop()
 
 
-
-
 if __name__ == "__main__":
     main()
-
-#
-#
-# " Test for duplicaates "
-# (it_data.groupby('id').agg(F.count("*").alias("count")).filter(F.col("count")>1))
-#
-#
-# (it_data.show())
-# (dataset_one.select("area").distinct().show())
-#
-#
-# dataset_one.show()
-#
-# dataset_two.show()
-#
-# dataset_three.show()
-#
-# dataset_two.select("name").distinct().show()
