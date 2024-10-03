97b898dd7f2ade011f717d15368c15b7ee256464
Michael Petrouleas <michaelpetrouleas@gmail.com>
2024-10-03 09:32:04 +0300

    Finalised task 3 script

---
+++ src/marketing_address_info.py
@@ -0,0 +1,65 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark import SparkConf
+import chispa
+from pyspark.sql.functions import column
+
+import importlib
+
+import utility.test_functions
+importlib.reload(utility.test_functions)
+from utility.test_functions import *
+
+
+def produce_department_breakdown_data(dataset_one_path, dataset_two_path, spark_session):
+
+    dataset_one = spark_session.read.csv(dataset_one_path, header=True, inferSchema=True)
+    dataset_two = spark_session.read.csv(dataset_two_path, header=True, inferSchema=True)
+
+    calls_per_department = dataset_one.groupBy('area').agg(
+        F.sum('calls_made').alias('total_calls_made'),
+        F.sum('calls_successful').alias('total_calls_successful')
+    ).withColumn('percentage_of_successful_calls',
+                 F.round((100 * F.col('total_calls_successful')) / F.col('total_calls_made'), 2))
+
+    sales_per_department = (dataset_one.select('id', 'area').join(
+        dataset_two.select('id', 'sales_amount'),
+        [dataset_one.id == dataset_two.id],
+        'left')
+                            .drop('id')
+                            .groupBy('area')
+                            .agg(F.sum('sales_amount').alias('total_sales_amount'))
+                            .withColumn('total_sales_amount', F.format_number(F.col('total_sales_amount'), 2))
+                            )
+
+    department_breakdown_data = (calls_per_department.join(sales_per_department,
+                                                           [calls_per_department.area == sales_per_department.area],
+                                                           'left')
+                                 .drop(sales_per_department.area))
+
+    return department_breakdown_data
+
+
+def main(dataset_one_path = r'../data/dataset_one.csv', dataset_two_path = r'../data/dataset_two.csv'):
+
+    spark = SparkSession.builder.getOrCreate()
+
+    try:
+
+        department_breakdown_data = produce_department_breakdown_data(dataset_one_path, dataset_two_path, spark_session=spark)
+
+        if test_for_non_logical_values(department_breakdown_data,column='total_sales_amount',condition='total_sales_amount<0'):
+            raise Exception("it_data:: Negative values were identified in the dataframe.")
+
+        if test_for_duplicate_entries(department_breakdown_data,identity_columns='area'):
+            raise Exception("it_data:: Duplicate entries were identified in the dataframe.")
+
+        department_breakdown_data.repartition(1).write.mode('overwrite').csv(path='../output/department_breakdown', header=True)
+
+    finally:
+
+        spark.stop()
+
+
+if __name__ == "__main__":
+    main()
